23354f0f58fed2b7b5594f81216417cef5ff3c29
Roger Boldu <roger.boldu@gmail.com>
2015-03-17 21:43:37 -0700

    Test code, is just a server, it works

---
+++ test/test/Serial.cpp
@@ -0,0 +1,54 @@
+#include "Serial.h"
+#include "stdafx.h"
+int Serial:: hey()
+{
+	
+	String^ answer;
+	String^ portName;
+	int baudRate=9600;
+	Console::WriteLine("Type in a port name and hit ENTER");
+	portName=Console::ReadLine();
+	// arduino settings
+	SerialPort^ arduino;
+	arduino = gcnew SerialPort(portName, baudRate);
+	// open port
+	try
+	{
+		arduino->Open();
+
+		do
+		{
+			// ask on or off
+			Console::WriteLine("Type \"on\" to turn the light on or \"off\" to turn it off");
+			// get answer
+			answer=Console::ReadLine();
+			//check that user typed one of the options
+			if(String::Compare(answer,"on")==0)
+				arduino->WriteLine("1"); // send 1 to arduino
+			else if(String::Compare(answer,"off")==0)
+				arduino->WriteLine("0"); // send 0 to arduino
+			else
+				Console::WriteLine(answer+" was not an option");
+			// ask user if he wants to continue
+			Console::WriteLine("Try again? yes/no");
+			// get answer
+			answer=Console::ReadLine();
+			// clear the screen
+			Console::Clear();
+		}while(String::Compare(answer,"yes")==0);
+		// close port to arduino
+		arduino->Close();
+	}
+	catch (IO::IOException^ e  ) 
+	{ 
+		Console::WriteLine(e->GetType()->Name+": Port is not ready");
+	}
+	catch (ArgumentException^ e)
+	{
+		Console::WriteLine(e->GetType()->Name+": incorrect port name syntax, must start with COM/com");
+	}
+	// end program
+	Console::Write("Press enter to close the program");
+	Console::Read();
+    return 0;
+}
---
+++ test/test/test.cpp
@@ -0,0 +1,312 @@
+﻿// ConsoleApplication1.cpp : Defines the entry point for the console application.
+//
+
+
+#include <Windows.h>
+#include <stdio.h>
+#include <conio.h>
+#include "stdafx.h"
+#include <assert.h>
+#include <tchar.h>
+#include <dos.h>
+//#include <synchapi.h>
+
+#undef UNICODE
+
+#define WIN32_LEAN_AND_MEAN
+
+#include <windows.h>
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+// Need to link with Ws2_32.lib
+#pragma comment (lib, "Ws2_32.lib")
+// #pragma comment (lib, "Mswsock.lib")
+
+#define DEFAULT_BUFLEN 512
+#define DEFAULT_PORT "27015"
+#define FREFRESH 10 //ms
+#define MAXNOSEND 5000 // 5sec
+#include <eyex\EyeX.h>
+
+#pragma comment (lib, "Tobii.EyeX.Client.lib")
+float x;
+float y;
+
+
+
+// ID of the global interactor that provides our data stream; must be unique within the application.
+static const TX_STRING InteractorId = "Twilight Sparkle";
+
+// global variables
+static TX_HANDLE g_hGlobalInteractorSnapshot = TX_EMPTY_HANDLE;
+
+bool InitializeGlobalInteractorSnapshot(TX_CONTEXTHANDLE hContext)
+{
+	TX_HANDLE hInteractor = TX_EMPTY_HANDLE;
+	TX_GAZEPOINTDATAPARAMS params = { TX_GAZEPOINTDATAMODE_LIGHTLYFILTERED };
+	bool success;
+
+	success = txCreateGlobalInteractorSnapshot(
+		hContext,
+		InteractorId,
+		&g_hGlobalInteractorSnapshot,
+		&hInteractor) == TX_RESULT_OK;
+	success &= txCreateGazePointDataBehavior(hInteractor, &params) == TX_RESULT_OK;
+
+	txReleaseObject(&hInteractor);
+
+	return success;
+}
+
+
+void TX_CALLCONVENTION OnSnapshotCommitted(TX_CONSTHANDLE hAsyncData, TX_USERPARAM param)
+{
+	// check the result code using an assertion.
+	// this will catch validation errors and runtime errors in debug builds. in release builds it won't do anything.
+
+	TX_RESULT result = TX_RESULT_UNKNOWN;
+	txGetAsyncDataResultCode(hAsyncData, &result);
+	assert(result == TX_RESULT_OK || result == TX_RESULT_CANCELLED);
+}
+
+void TX_CALLCONVENTION OnEngineConnectionStateChanged(TX_CONNECTIONSTATE connectionState, TX_USERPARAM userParam)
+{
+	switch (connectionState) {
+	case TX_CONNECTIONSTATE_CONNECTED: {
+			bool success;
+			printf("The connection state is now CONNECTED (We are connected to the EyeX Engine)\n");
+			// commit the snapshot with the global interactor as soon as the connection to the engine is established.
+			// (it cannot be done earlier because committing means "send to the engine".)
+			success = txCommitSnapshotAsync(g_hGlobalInteractorSnapshot, OnSnapshotCommitted, NULL) == TX_RESULT_OK;
+			if (!success) {
+				printf("Failed to initialize the data stream.\n");
+			}
+			else {
+				printf("Waiting for gaze data to start streaming...\n");
+			}
+		}
+		break;
+
+	case TX_CONNECTIONSTATE_DISCONNECTED:
+		printf("The connection state is now DISCONNECTED (We are disconnected from the EyeX Engine)\n");
+		break;
+
+	case TX_CONNECTIONSTATE_TRYINGTOCONNECT:
+		printf("The connection state is now TRYINGTOCONNECT (We are trying to connect to the EyeX Engine)\n");
+		break;
+
+	case TX_CONNECTIONSTATE_SERVERVERSIONTOOLOW:
+		printf("The connection state is now SERVER_VERSION_TOO_LOW: this application requires a more recent version of the EyeX Engine to run.\n");
+		break;
+
+	case TX_CONNECTIONSTATE_SERVERVERSIONTOOHIGH:
+		printf("The connection state is now SERVER_VERSION_TOO_HIGH: this application requires an older version of the EyeX Engine to run.\n");
+		break;
+	}
+}
+
+
+
+void OnGazeDataEvent(TX_HANDLE hGazeDataBehavior)
+{
+	TX_GAZEPOINTDATAEVENTPARAMS eventParams;
+	if (txGetGazePointDataEventParams(hGazeDataBehavior, &eventParams) == TX_RESULT_OK) {
+		//printf("Gaze Dataaaaaa: (%.1f, %.1f) timestamp %.0f ms\n", eventParams.X, eventParams.Y, eventParams.Timestamp);
+		x=(float)eventParams.X;
+		y=(float)eventParams.Y;
+	} else {
+		
+		printf("Failed to interpret gaze data event packet.\n");
+	}
+}
+
+
+void TX_CALLCONVENTION HandleEvent(TX_CONSTHANDLE hAsyncData, TX_USERPARAM userParam)
+{
+	TX_HANDLE hEvent = TX_EMPTY_HANDLE;
+	TX_HANDLE hBehavior = TX_EMPTY_HANDLE;
+
+	txGetAsyncDataContent(hAsyncData, &hEvent);
+
+	// NOTE. Uncomment the following line of code to view the event object. The same function can be used with any interaction object.
+	//OutputDebugStringA(txDebugObject(hEvent));
+
+	if (txGetEventBehavior(hEvent, &hBehavior, TX_BEHAVIORTYPE_GAZEPOINTDATA) == TX_RESULT_OK) {
+		OnGazeDataEvent(hBehavior);
+		txReleaseObject(&hBehavior);
+	}
+
+	// NOTE since this is a very simple application with a single interactor and a single data stream, 
+	// our event handling code can be very simple too. A more complex application would typically have to 
+	// check for multiple behaviors and route events based on interactor IDs.
+
+	txReleaseObject(&hEvent);
+}
+
+using namespace System;
+using namespace System::IO::Ports;
+
+
+
+
+
+int _tmain(int argc, _TCHAR* argv[])
+{   
+	WSADATA wsaData;
+    int iResult;
+
+    SOCKET ListenSocket = INVALID_SOCKET;
+    SOCKET ClientSocket = INVALID_SOCKET;
+
+    struct addrinfo *result = NULL;
+    struct addrinfo hints;
+
+    int iSendResult;
+    char recvbuf[DEFAULT_BUFLEN];
+    int recvbuflen = DEFAULT_BUFLEN;
+    
+    // Initialize Winsock
+    iResult = WSAStartup(MAKEWORD(2,2), &wsaData);
+    if (iResult != 0) {
+        printf("WSAStartup failed with error: %d\n", iResult);
+        return 1;
+    }
+
+    ZeroMemory(&hints, sizeof(hints));
+    hints.ai_family = AF_INET;
+    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_protocol = IPPROTO_TCP;
+    hints.ai_flags = AI_PASSIVE;
+
+    // Resolve the server address and port
+    iResult = getaddrinfo(NULL, DEFAULT_PORT, &hints, &result);
+    if ( iResult != 0 ) {
+        printf("getaddrinfo failed with error: %d\n", iResult);
+        WSACleanup();
+        return 1;
+    }
+
+    // Create a SOCKET for connecting to server
+    ListenSocket = socket(result->ai_family, result->ai_socktype, result->ai_protocol);
+    if (ListenSocket == INVALID_SOCKET) {
+        printf("socket failed with error: %ld\n", WSAGetLastError());
+        freeaddrinfo(result);
+        WSACleanup();
+        return 1;
+    }
+
+    // Setup the TCP listening socket
+    iResult = bind( ListenSocket, result->ai_addr, (int)result->ai_addrlen);
+    if (iResult == SOCKET_ERROR) {
+        printf("bind failed with error: %d\n", WSAGetLastError());
+        freeaddrinfo(result);
+        closesocket(ListenSocket);
+        WSACleanup();
+        return 1;
+    }
+
+    freeaddrinfo(result);
+
+    iResult = listen(ListenSocket, SOMAXCONN);
+    if (iResult == SOCKET_ERROR) {
+        printf("listen failed with error: %d\n", WSAGetLastError());
+        closesocket(ListenSocket);
+        WSACleanup();
+        return 1;
+    }
+
+
+
+    // No longer need server socket
+  
+    // Receive until the peer shuts down the connection
+ 
+
+
+
+
+	TX_CONTEXTHANDLE hContext = TX_EMPTY_HANDLE;
+	TX_TICKET hConnectionStateChangedTicket = TX_INVALID_TICKET;
+	TX_TICKET hEventHandlerTicket = TX_INVALID_TICKET;
+	bool success;
+
+	// initialize and enable the context that is our link to the EyeX Engine.
+	success = txInitializeEyeX(TX_EYEXCOMPONENTOVERRIDEFLAG_NONE, NULL, NULL, NULL, NULL) == TX_RESULT_OK;
+	success &= txCreateContext(&hContext, TX_FALSE) == TX_RESULT_OK;
+	success &= InitializeGlobalInteractorSnapshot(hContext);
+	success &= txRegisterConnectionStateChangedHandler(hContext, &hConnectionStateChangedTicket, OnEngineConnectionStateChanged, NULL) == TX_RESULT_OK;
+	success &= txRegisterEventHandler(hContext, &hEventHandlerTicket, HandleEvent, NULL) == TX_RESULT_OK;
+	success &= txEnableConnection(hContext) == TX_RESULT_OK;
+
+	// let the events flow until a key is pressed.
+	if (success) {
+		printf("Initialization was successful.\n");
+	} else {
+		printf("Initialization failed.\n");
+	}
+
+	char aux[50];
+	float old_x;
+	float old_y;
+	char order;
+	int counter=0;
+	while(1){
+	    ClientSocket = accept(ListenSocket, NULL, NULL);
+	 if (ClientSocket == INVALID_SOCKET) {
+        printf("accept failed with error: %d\n", WSAGetLastError());
+        closesocket(ListenSocket);
+        WSACleanup();
+        return 1;
+    }
+		   do {
+			 
+
+		if (old_x!=x && old_y!=y){
+			counter=0;
+			printf("x=%0.1f,y=%0.1f\n",x,y);
+			old_x=x;
+			old_y=y;
+			sprintf(aux,"%0.1f,%0.1f",x,y);
+            iSendResult = send( ClientSocket, aux, strlen(aux), 0 );
+            //printf("Bytes sent: %d\n", iSendResult);
+	
+		}else{
+			counter++;
+			_sleep(FREFRESH);
+			// until error
+			if (counter>MAXNOSEND/FREFRESH){
+				sprintf(aux,"%0.1f,%0.1f",x,y);
+				iSendResult = send( ClientSocket, aux, strlen(aux), 0 );
+				printf("-------Sending same info-------\n", iSendResult);
+				counter=0;
+			}
+		}} while (iSendResult != SOCKET_ERROR);
+		closesocket(ClientSocket);
+	}
+
+    // shutdown the connection since we're done
+    iResult = shutdown(ClientSocket, SD_SEND);
+    if (iResult == SOCKET_ERROR) {
+        printf("shutdown failed with error: %d\n", WSAGetLastError());
+        closesocket(ClientSocket);
+        WSACleanup();
+        return 1;
+    }
+	 closesocket(ClientSocket);
+    WSACleanup();
+
+	// disable and delete the context.
+	txDisableConnection(hContext);
+	txReleaseObject(&g_hGlobalInteractorSnapshot);
+	success = txShutdownContext(hContext, TX_CLEANUPTIMEOUT_DEFAULT, TX_FALSE) == TX_RESULT_OK;
+	success &= txReleaseContext(&hContext) == TX_RESULT_OK;
+	success &= txUninitializeEyeX() == TX_RESULT_OK;
+	if (!success) {
+		printf("EyeX could not be shut down cleanly. Did you remember to release all handles?\n");
+	}
+	return 0;
+		   
+}
